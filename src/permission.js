c571dee9b6bc415cf51c76a22cc39ac60e7cd7f4
imjustpassby <260809970@qq.com>
2019-04-11 21:43:23 +0800

    permission 添加路由权限

--- src/permission.js
+++ src/permission.js
@@ -1,36 +1,57 @@
 import router from './router'
 import store from './store'
 import NProgress from 'nprogress' // Progress 进度条
-import 'nprogress/nprogress.css'// Progress 进度条样式
-import { Message } from 'element-ui'
-import { getToken } from '@/utils/auth' // 验权
+import 'nprogress/nprogress.css' // Progress 进度条样式
+import {
+  Message,
+} from 'element-ui'
+import {
+  getToken
+} from '@/utils/auth' // 验权
 
 const whiteList = ['/login'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
   NProgress.start()
   if (getToken()) {
     if (to.path === '/login') {
-      next({ path: '/' })
+      next({
+        path: '/'
+      })
       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
       if (store.getters.roles.length === 0) {
         store.dispatch('GetInfo').then(res => { // 拉取用户信息
-          next()
+          next();
         }).catch((err) => {
           store.dispatch('FedLogOut').then(() => {
             Message.error(err || 'Verification failed, please login again')
-            next({ path: '/' })
+            next({
+              path: '/'
+            })
           })
         })
       } else {
-        next()
+        let hasPermission = to.path.match(/report|payment|staff/);
+        if (hasPermission) {
+          if (store.getters.roles.includes('admin')) {
+            NProgress.done()
+            next()
+          } else {
+            Message.error('NO PERMISSION')
+            next('/dashboard')
+            NProgress.done()
+          }
+        } else {
+          next();
+          NProgress.done()
+        }
       }
     }
   } else {
     if (whiteList.indexOf(to.path) !== -1) {
       next()
     } else {
-      next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
+      next(`/login`) // 否则全部重定向到登录页
       NProgress.done()
     }
   }
